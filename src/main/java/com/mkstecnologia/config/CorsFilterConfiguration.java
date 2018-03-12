80be1b02d1ed87afe297508c2eb543064b4ab1fd
markussouza <markussouza@192.168.0.15>
2018-03-12 20:52:29 -0300

    Ajuste na classe de configuração

--- src/main/java/com/mkstecnologia/config/CorsFilterConfiguration.java
+++ src/main/java/com/mkstecnologia/config/CorsFilterConfiguration.java
@@ -3,10 +3,8 @@
  */
 package com.mkstecnologia.config;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -22,7 +20,7 @@ import org.springframework.web.filter.CorsFilter;
 public class CorsFilterConfiguration {
 
     @Bean
-    public FilterRegistrationBean corsFilter() {
+    public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
@@ -30,9 +28,7 @@ public class CorsFilterConfiguration {
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
-        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        return bean;
+        return new CorsFilter(source);
     }
 
 }
